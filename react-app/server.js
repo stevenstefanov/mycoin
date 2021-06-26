c605e8c4c94bb0d362a36fcdae2708e0bdf77bc6
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-06-26 12:08:08 -0400

    pusing updated server.js with sequel code

--- react-app/server.js
+++ react-app/server.js
@@ -1,43 +1,33 @@
-// Define required packages
+const path = require('path');
 const express = require('express');
-// const path = require('path');
-const mongoose = require('mongoose');
-const routes = require("./routes/api");
+const session = require('express-session');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// Define middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve up static assets 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
-
-// Use routes
 app.use(routes);
 
-// Connect to the Mongo Database
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/reactcrypto',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
-);
-
-// Send every request to the React app
-// app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
-
-// Start the API server
-app.listen(PORT, function() {
-    console.log(`App running on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
 });
