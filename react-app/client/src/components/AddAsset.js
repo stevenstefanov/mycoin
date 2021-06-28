536b209f0eaf550ba73d8668a09b41002f5eee5e
Justin Ng <ng.justin07@gmail.com>
2021-06-28 15:36:15 -0400

    commit

--- react-app/client/src/components/AddAsset.js
+++ react-app/client/src/components/AddAsset.js
@@ -7,13 +7,13 @@ export default function AddAsset() {
                 <div class="form-group row">
                     <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
                     <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email"/>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
                     <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password"/>
                     </div>
                 </div>
                 <fieldset class="form-group">
@@ -21,19 +21,19 @@ export default function AddAsset() {
                     <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
                     <div class="col-sm-10">
                         <div class="form-check">
-                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked/>
                         <label class="form-check-label" for="gridRadios1">
                             First radio
                         </label>
                         </div>
                         <div class="form-check">
-                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2"/>
                         <label class="form-check-label" for="gridRadios2">
                             Second radio
                         </label>
                         </div>
                         <div class="form-check disabled">
-                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled/>
                         <label class="form-check-label" for="gridRadios3">
                             Third disabled radio
                         </label>
@@ -45,7 +45,7 @@ export default function AddAsset() {
                     <div class="col-sm-2">Checkbox</div>
                     <div class="col-sm-10">
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck1">
+                        <input class="form-check-input" type="checkbox" id="gridCheck1"/>
                         <label class="form-check-label" for="gridCheck1">
                         Example checkbox
                         </label>
