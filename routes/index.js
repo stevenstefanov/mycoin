77d40003ec5a6b35db9429257040f4a5f5dd4171
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-07-10 14:51:17 -0400

    heroku deployment fix

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-const router = require('express').Router();
-const path = require('path');
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const path = require("path");
+const apiRoutes = require("./api");
 
 console.log("OUTER INDEX");
 
 // API Routes
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
 
-if ( process.env.NODE_ENV === "production" ) {
-    router.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-  }
+if (process.env.NODE_ENV === "production") {
+  router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
 
 module.exports = router;
