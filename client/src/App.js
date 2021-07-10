5d5736677706daf77afeac9ae96fa28f03bf985d
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-07-10 10:58:44 -0400

    news date sorted and image populated + added prettier formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,54 @@
-import React, { useState } from 'react'
-import './App.css';
-import Nav from './components/Nav'
-import Portfolio from './components/Portfolio'
-import Three from './Utils/Three.js'
-import Chart from './Utils/charts';
-import AddAsset from './components/AddAsset'
-import SignUp from './components/SignUp'
-import Login from './components/Login'
-import Coin from './components/Rankings'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import News from './components/News';
-
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Portfolio from "./components/Portfolio";
+import Three from "./Utils/Three.js";
+import Chart from "./Utils/charts";
+import AddAsset from "./components/AddAsset";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Coin from "./components/Rankings";
+import News from "./components/News";
+import "./App.css";
+import "./assets/css/style.css";
 
 function App() {
-  const [assets, setAssets] = useState([])
+  const [assets, setAssets] = useState([]);
 
   return (
     <Router>
-    
       <Switch>
-      
-      <div className="App">
-      <Route exact path ='/'>
-        <Three></Three>
-      </Route>
+        <div className="App">
+          <Route exact path="/">
+            <Three></Three>
+          </Route>
 
-  
-      <Nav />
-      <Route exact path = '/home'>
-      <AddAsset/>
-      </Route>
-      
-      <Route exact path = '/portfolio'>
-      <Portfolio assets = {assets}/>
-      <Chart />
-      </Route>
+          <Nav />
+          <Route exact path="/home">
+            <AddAsset />
+          </Route>
 
-      <Route exact path = '/rankings'>
-      <Coin/>
-      </Route>
+          <Route exact path="/portfolio">
+            <Portfolio assets={assets} />
+            <Chart />
+          </Route>
 
-      <Route exact path = '/signup'>
-        <SignUp/>
-      </Route>
+          <Route exact path="/rankings">
+            <Coin />
+          </Route>
 
-      <Route exact path ='/login'>
-        <Login/>
-      </Route>
-      <Route exact path ='/news'>
-        <News />
-      </Route>
-      </div>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
 
-    
-    </Switch>
-
-    
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/news">
+            <News />
+          </Route>
+        </div>
+      </Switch>
     </Router>
   );
 }
