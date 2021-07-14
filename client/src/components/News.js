986da53bbd8962a0bd84a3b31854361390f8db41
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-07-14 19:36:26 -0400

    news page styled

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -44,35 +44,45 @@ function News() {
   }, []);
 
   return (
-    <div>
-      <input
-        type="text"
-        name="search"
-        value={search}
-        onChange={(e) => {
-          handleInputChange(e);
-        }}
-      />
-      <button onClick={handleSubmit}>Search</button>
-      {newsData.length > 0 ? (
-        newsData.map((data) => {
-          return (
-            <article>
-              <img src={data.image} alt="" />
-              <h5>
-                <a href={data.url} target="_blank" rel="noreferrer">
-                  {data.title}
-                </a>
-              </h5>
-              <p>Author: {data.author === null ? "N/A" : data.author} </p>
-              <p>Published: {formatDate(data.published_at)}</p>
-              <p>{data.description}</p>
-            </article>
-          );
-        })
-      ) : (
-        <div>no data</div>
-      )}
+    <div className="container news-section">
+      <div className="news-card">
+        <input
+          className="news-search"
+          type="text"
+          name="search"
+          value={search}
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+        />
+        <button
+          className="btn btn-outline-light btn-sm news-btn"
+          onClick={handleSubmit}
+        >
+          Search
+        </button>
+        {newsData.length > 0 ? (
+          newsData.map((data) => {
+            return (
+              <article className="news-content">
+                <br></br>
+                <h5>
+                  <a href={data.url} target="_blank" rel="noreferrer">
+                    {data.title}
+                  </a>
+                </h5>
+                <p>Author: {data.author === null ? "N/A" : data.author} </p>
+                <p>Published: {formatDate(data.published_at)}</p>
+                <p>{data.description}</p>
+                <img src={data.image} alt="" />
+                <hr></hr>
+              </article>
+            );
+          })
+        ) : (
+          <div>no data</div>
+        )}
+      </div>
     </div>
   );
 }
