a2354a1be46633f23797076d5ebee6a8ae46e8c5
fewb92 <sotojosh96@gmail.com>
2021-07-13 21:09:50 -0400

    added sold functoinality

--- client/src/components/Rankings.js
+++ client/src/components/Rankings.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { indexOf } from "lodash";
 
 function Rankings() {
   // const [search, setSearch] = useState('')
